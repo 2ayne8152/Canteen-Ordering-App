9f47db2802a5ac5a8c159c52f787f7a6e003a01d
edwardtee <edwardtee061118@gmail.com>
2025-12-10 18:27:07 +0800

    NavHost

--- app/src/main/java/com/example/canteen/MainActivity.kt
+++ app/src/main/java/com/example/canteen/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.canteen.ui.screens.CanteenApp
 import com.example.canteen.ui.screens.loginscreens.LoginScreen
 import com.example.canteen.ui.screens.loginscreens.StaffLoginScreen
 import com.example.canteen.ui.screens.staffMenu.MenuItemForm
@@ -46,57 +47,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun CanteenApp() {
-    val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = "login") {
-        // User Login Screen
-        composable("login") {
-            LoginScreen(
-                onStaffLoginClick = { navController.navigate("staff_login") },
-                onLoginSuccess = { role ->
-                    when (role) {
-                        "user" -> navController.navigate("user_menu") {
-                            popUpTo("login") { inclusive = true }
-                        }
-                        "staff" -> navController.navigate("staff_menu") {
-                            popUpTo("login") { inclusive = true }
-                        }
-                    }
-                }
-            )
-        }
-
-        // Staff Login Screen
-        composable("staff_login") {
-            StaffLoginScreen(
-                onUserLoginClick = { navController.navigate("login") },
-                onLoginSuccess = { role ->
-                    when (role) {
-                        "staff" -> navController.navigate("staff_menu") {
-                            popUpTo("staff_login") { inclusive = true }
-                        }
-                        "user" -> navController.navigate("user_menu") {
-                            popUpTo("staff_login") { inclusive = true }
-                        }
-                    }
-                }
-            )
-        }
-
-        // User Menu Screen
-        /*composable("user_menu") {
-            UserMenu(
-                onDetailClick = {
-                    // TODO: Navigate to order details/cart screen
-                }
-            )
-        }*/
-
-        // Staff Menu Screen
-        composable("staff_menu") {
-            StaffDashboardScreen()
-        }
-    }
-}
