8dab13d2670548097eaf59ddf09e16b2b4d1dc3b
edwardtee <edwardtee061118@gmail.com>
2025-12-17 16:37:16 +0800

    modify

--- app/src/main/java/com/example/canteen/Repository/OrderRepository.kt
+++ app/src/main/java/com/example/canteen/Repository/OrderRepository.kt
@@ -2,68 +2,12 @@ package com.example.canteen.repository
 
 import com.example.canteen.data.CartItem
 import com.example.canteen.data.Order
-import com.example.canteen.data.dao.OrderDao
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.Query
 import com.google.protobuf.LazyStringArrayList.emptyList
 import kotlinx.coroutines.tasks.await
 import kotlin.collections.emptyList
-
-/*class OrderRepository(
-    private val dao: OrderDao = OrderDao()
-) {
-
-    suspend fun createOrder(
-        userId: String,
-        items: List<CartItem>,
-        totalAmount: Double
-    ): Order {
-        val orderId = dao.ordersCollection.document().id
-
-        val order = Order(
-            orderId = orderId,
-            userId = userId,
-            items = items,
-            totalAmount = totalAmount,
-            status = "PENDING"
-        )
-
-        dao.insertOrder(order)
-        return order
-    }
-
-    suspend fun markOrderPaid(orderId: String) {
-        dao.updateOrderStatus(orderId, "PAID")
-    }
-
-    suspend fun getOrder(orderId: String): Order? {
-        return dao.getOrderById(orderId)
-    }
-
-    suspend fun getOrdersByUser(userId: String): List<Order> {
-        return dao.getOrdersByUser(userId)
-    }
-
-    fun listenOrdersByUserId(
-        userId: String,
-        onUpdate: (List<Order>) -> Unit
-    ): ListenerRegistration {
-
-        return FirebaseFirestore.getInstance()
-            .collection("orders")
-            .whereEqualTo("userId", userId)
-            .addSnapshotListener { snapshot, error ->
-                if (error != null) return@addSnapshotListener
-
-                val orders = snapshot?.documents?.map {
-                    Order.fromMap(it.data!!)
-                } ?: emptyList()
-
-                onUpdate(orders)
-            }
-    }
-}*/
 
 class OrderRepository {
 
